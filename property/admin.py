f4efa095d3b00f4c29a5cb7779afc95b405dae69
Andrey Miskov <amiskov@gmail.com>
2023-08-20 11:49:36 +0300

    setup Flat changelist page

--- property/admin.py
+++ property/admin.py
@@ -4,8 +4,11 @@ from .models import Flat
 
 
 class FlatAdmin(admin.ModelAdmin):
-    search_fields = ['town', 'town_district', 'address', 'owner']
-    readonly_fields = ['created_at']
+    search_fields = ('town', 'town_district', 'address', 'owner')
+    readonly_fields = ('created_at',)
+    list_display = ('address', 'price', 'new_building',
+                    'construction_year', 'town')
+    list_editable = ('new_building',)
 
 
 admin.site.register(Flat, FlatAdmin)
